1fa4408c34d4a9d6fd38da1850fe4e88e41d3ed3
Miguel <miguelvisintini1@gmail.com>
2023-10-14 03:40:11 -0300

    base reproductor de audios

--- src/PodcastPlayerApp.jsx
+++ src/PodcastPlayerApp.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SearchBar } from './components/SearchBar'
 import { PodcastGrid } from './components/PodcastGrid'
 import { PodcastDetailsGrid } from './components/PodcastDetailsGrid';
+import { AudioPlayer } from './components/AudioPlayer';
 
 export const PodcastPlayerApp = () => {
 
@@ -16,21 +17,23 @@ export const PodcastPlayerApp = () => {
 
   return (
 
-    <BrowserRouter>
-      <Routes>
+    // <BrowserRouter>
+    //   <Routes>
 
-        <Route exact path="/" element={
-          <div className="flex-col mx-auto w-830">
-            <SearchBar onSetSearch={ onSetSearch }/>
-            <PodcastGrid search={ search }/>
-          </div>
-        }/>
+    //     <Route exact path="/" element={
+    //       <div className="flex-col mx-auto w-830">
+    //         <SearchBar onSetSearch={ onSetSearch }/>
+    //         <PodcastGrid search={ search }/>
+    //       </div>
+    //     }/>
 
-        <Route path="/podcast/:collectionId" element={
-          <PodcastDetailsGrid />
-        }/>
+    //     <Route path="/podcast/:collectionId" element={
+    //       <PodcastDetailsGrid />
+    //     }/>
 
-      </Routes>
-    </BrowserRouter>
+    //   </Routes>
+    // </BrowserRouter>
+
+    <AudioPlayer />
   )
 }
